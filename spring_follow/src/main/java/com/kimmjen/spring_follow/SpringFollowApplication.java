696d9c407232c3d678c4eb1684a35856ed551954
kimmjen <wpals814@gmail.com>
2021-02-01 21:59:22 +0900

    오류 고쳐보기

---
+++ spring_follow/src/main/java/com/kimmjen/spring_follow/SpringFollowApplication.java
@@ -0,0 +1,13 @@
+package com.kimmjen.spring_follow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFollowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFollowApplication.class, args);
+	}
+
+}
---
+++ spring_follow/src/test/java/com/kimmjen/spring_follow/SpringFollowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimmjen.spring_follow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFollowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
