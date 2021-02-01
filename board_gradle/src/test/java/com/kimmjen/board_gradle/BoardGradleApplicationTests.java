934d5acb5764f90494ff8b1f92d8c9ec4a740cca
kimmjen <wpals814@gmail.com>
2021-02-01 15:50:46 +0900

    게시판_그래들 설정

---
+++ board_gradle/src/test/java/com/kimmjen/board_gradle/BoardGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimmjen.board_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
