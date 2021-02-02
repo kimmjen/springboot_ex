df2fdc784cab4543dc2dce004d4f40d59fe953b6
kimmjen <wpals814@gmail.com>
2021-02-02 10:50:56 +0900

    게시글 등록 구현 완료

---
+++ spring_follow/src/main/java/com/kimmjen/spring_follow/controller/BoardController.java
@@ -0,0 +1,80 @@
+package com.kimmjen.spring_follow.controller;
+
+import com.kimmjen.spring_follow.domain.BoardDTO;
+import com.kimmjen.spring_follow.service.BoardService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    // @GetMapping(value = "/board/write.do")
+    // public String openBoardWrite(Model model) {
+    //     return "board/service";
+    // }
+    // @GetMapping(value = "/board/write.do")
+	// public String openBoardWrite(Model model) {
+
+	// 	String title = "제목";
+	// 	String content = "내용";
+	// 	String writer = "홍길동";
+
+	// 	model.addAttribute("t", title);
+	// 	model.addAttribute("c", content);
+	// 	model.addAttribute("w", writer);
+
+	// 	return "board/write";
+	// }
+	@GetMapping(value = "/board/write.do")
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			model.addAttribute("board", new BoardDTO());
+		} else {
+			BoardDTO board = boardService.getBoardDetail(idx);
+			if (board == null) {
+				return "redirect:/board/list.do";
+			}
+			model.addAttribute("board", board);
+		}
+
+		return "board/write";
+	}
+	@PostMapping(value = "/board/register.do")
+	public String registerBoard(final BoardDTO params) {
+		try {
+			boolean isRegistered = boardService.registerBoard(params);
+			if (isRegistered == false) {
+				// TODO => 게시글 등록에 실패하였다는 메시지를 전달
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
+		} catch (Exception e) {
+			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+		}
+
+		return "redirect:/board/list.do";
+	}
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
